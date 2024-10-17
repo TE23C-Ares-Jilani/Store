b4852820463d1e7172e4c568c8d23d52e636c135
Ares Jilani <ares.jilani@elev.ga.ntig>
2024-10-17 13:09:24 +0200

    dsada

--- Store/Program.cs
+++ Store/Program.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.VisualBasic;
+﻿
+using System.Security.Cryptography.X509Certificates;
 
 int playerMoney = 100;
 
 while (playerMoney > 0)
 {
-    Console.WriteLine("welcome to the diddy shop.");
+    Console.WriteLine("This is the diddy shop.");
     Console.WriteLine($"you got {playerMoney} diddy coins");
 
     // vad man kan köpa
@@ -12,12 +13,16 @@ while (playerMoney > 0)
     Console.WriteLine("You can buy:");
     Console.WriteLine("1. toilet (10 dcoins)");
     Console.WriteLine("2. baby oil (20 dcoins)");
-    Console.WriteLine("3. drugs (30 dcoins)");
+    Console.WriteLine("3. barny dust (30 dcoins)");
 
     string buy = "";
     while (buy != "1" && buy != "2" && buy != "3")
     {
         buy = Console.ReadLine();
+        if (buy != "1" && buy != "2" && buy != "3")
+        {
+            Console.WriteLine("thats not an item dipshit!");
+        }
     }
 
     //price per item
@@ -27,39 +32,40 @@ while (playerMoney > 0)
     {
         pricePerItem = 10;
     }
-    if (buy == "2")
+    else if (buy == "2")
     {
         pricePerItem = 20;
     }
-    if (buy == "3")
+    else if (buy == "3")
     {
         pricePerItem = 30;
     }
+    
     // How many buy
 
-    Console.WriteLine("Hur många vill du köpa?");
-    int howMany = 0;
-    while (howMany == 0)
-    {
-        string num = Console.ReadLine();
-        bool success = int.TryParse(num, out howMany);
-        if (success == false)
+        Console.WriteLine("Hur många vill du köpa?");
+        int howMany = 0;
+        while (howMany == 0)
         {
-            Console.WriteLine("write a number dickhead");
+            string num = Console.ReadLine();
+            bool success = int.TryParse(num, out howMany);
+            if (success == false || howMany <= 1)
+            {
+                Console.WriteLine("stop being a dumbass and give a real answer");
+                pricePerItem = 0;
+            }
+        }
+        int finalCost = pricePerItem * howMany;
+        if (finalCost <= playerMoney && finalCost >= 1)
+        {
+            Console.WriteLine("Ight that works");
+            playerMoney -= finalCost;
+        }
+        else if (finalCost >= playerMoney)
+        {
+            Console.WriteLine("Nah, you to poor for that");
         }
     }
-    int finalCost = pricePerItem * howMany;
-    if (finalCost <= playerMoney)
-    {
-        Console.WriteLine("Ight that works");
-        playerMoney -= finalCost;
-    }
-else
-    {
-        Console.WriteLine("Nah, you to poor for that");
-}
-}
-
 Console.WriteLine("You out of money now");
 Console.WriteLine("cya later. press ENTER to close");
 Console.ReadKey();
